53d606c35e96a48dfdb94dbb13d386323e5c7d9c
Daixuan <huodx@ucla.edu>
2024-03-16 22:37:47 -0700

    fasterspinning

--- models/coin.js
+++ models/coin.js
@@ -7,7 +7,7 @@ const { Mat4 } = tiny;
 /* The reason for doing this is to avoid referencing Examples */
 export class CoinShape extends TexturedModel {
     constructor(textureName) {
-        super('/assets/coin.obj','/assets/coin.png',{ ambient: 0.4, diffusivity: 0.3});
+        super('/assets/coin.obj','/assets/coin.png',{ ambient: 0.5, diffusivity: 0.3});
         this.phase = Math.random();
     }
     draw(caller, uniforms, model_transform) {               // draw(): Same as always for shapes, but cancel all
@@ -19,7 +19,7 @@ export class CoinShape extends TexturedModel {
         model_transform.pre_multiply(Mat4.translation(0,0.4 + 
             Math.sin((2+this.phase/2)*Math.PI*t + this.phase)/4 // realistic hover
             ,0));
-        model_transform = model_transform.times(Mat4.rotation(t,0,1,0));
+        model_transform = model_transform.times(Mat4.rotation(1.5*t,0,1,0));
         model_transform = model_transform.times(Mat4.scale(2,2,2));
         super.draw(caller, uniforms, model_transform);
     }
